a9765237d52a0829ce44d50e0a6a710aca53f643
wanghui5801 <huiw1128@gmail.com>
2024-07-25 12:32:26 -0400

    Update Readme.md

--- R/data.R
+++ R/data.R
@@ -6,3 +6,12 @@
 #' @examples
 #' head(climate)
 "climate"
+
+#' Stock index
+#'
+#' This is a time series of stock series which contains AAPL and MSFT from March 13, 1986 to August 6, 2018.
+#'
+#'
+#' @examples
+#' head(stock)
+"stock"
